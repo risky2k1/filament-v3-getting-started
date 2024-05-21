2e182f998811e385d746ea8dea848e8cfd659cea
riskyyy <theloneranger241@gmail.com>
2024-05-21 15:12:57 +0700

    Add relationship for the models

--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Owner extends Model
 {
@@ -14,4 +15,9 @@ class Owner extends Model
         'name',
         'phone',
     ];
+
+    public function patients(): HasMany
+    {
+        return $this->hasMany(Patient::class);
+    }
 }
--- app/Models/Treatment.php
+++ app/Models/Treatment.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Treatment extends Model
 {
@@ -15,4 +16,9 @@ class Treatment extends Model
         'patient_id',
         'price',
     ];
+
+    public function patient(): BelongsTo
+    {
+        return $this->belongsTo(Patient::class);
+    }
 }
