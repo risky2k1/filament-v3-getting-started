23cf1c0feadd243fad0a818b2accb6ea3e550499
riskyyy <theloneranger241@gmail.com>
2024-05-21 17:04:40 +0700

    show treatment table

--- app/Models/Treatment.php
+++ app/Models/Treatment.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Casts\MoneyCast;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -15,6 +16,9 @@ class Treatment extends Model
         'notes',
         'patient_id',
         'price',
+    ];
+    protected $casts = [
+        'price' => MoneyCast::class,
     ];
 
     public function patient(): BelongsTo
